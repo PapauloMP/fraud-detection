f8930ab2147a2a9ad59915a98c62b3a775f0c57e
Marcos Paulo <marcospaulo.rodrigues@estudante.ufjf.br>
2026-06-04 08:48:39 -0300

    Adding inference for test dataset and LIME

--- src/inference.py
+++ src/inference.py
@@ -4,13 +4,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+import webbrowser
 from transformers import AutoTokenizer, AutoModel
+from lime.lime_text import LimeTextExplainer
+from datetime import datetime
 
 MODEL_NAME = "neuralmind/bert-base-portuguese-cased"
 MAX_LEN = 512
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 WEIGHTS_PATH = "generation/outputs/models/best_model_no_metrics_20260521_031048.pt"
 CONFIG_PATH = WEIGHTS_PATH.replace(".pt", "_config.json")
+LIME_OUTPUT_DIR = "generation/outputs/analysis/"
+
+os.makedirs(os.path.dirname(LIME_OUTPUT_DIR), exist_ok=True)
 
 class BERTClassifier(nn.Module):
     def __init__(self, model_name, num_classes):
@@ -72,6 +78,28 @@ def predict_text(text, model, tokenizer, temperature, thresholds):
     
     return probs, predict_class
 
+def make_lime_predict_fn(model, tokenizer, temperature):
+    def predict_fn(texts):
+        all_probs = []
+        batch_size = 16 
+        model.eval()
+        for i in range(0, len(texts), batch_size):
+            batch = texts[i:i + batch_size]
+            inputs = tokenizer(batch, truncation=True, padding=True, max_length=MAX_LEN, return_tensors="pt")
+            
+            input_ids = inputs["input_ids"].to(DEVICE)
+            attention_mask = inputs["attention_mask"].to(DEVICE)
+
+            with torch.no_grad():
+                logits = model(input_ids, attention_mask)
+                calibrated_logits = logits / temperature
+                probs = F.softmax(calibrated_logits, dim=1)
+                
+            all_probs.extend(probs.cpu().numpy())
+            
+        return np.array(all_probs)
+    return predict_fn
+
 def main():
     print("="*50)
     print("Carregando...")
@@ -91,9 +119,14 @@ def main():
 
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
     model = BERTClassifier(MODEL_NAME, num_classes=3)
-
-    model.load_state_dict(torch.load(WEIGHTS_PATH, map_location=DEVICE))
+    model.load_state_dict(torch.load(WEIGHTS_PATH, map_location=DEVICE, weights_only=True))
     model.to(DEVICE)
+
+    # Inicializa o Explicador LIME
+    labels_map = {0: "Humano", 1: "IA", 2: "Copy-Typing"}
+    class_names = [labels_map[0], labels_map[1], labels_map[2]]
+    explainer = LimeTextExplainer(class_names=class_names)
+    lime_predict_fn = make_lime_predict_fn(model, tokenizer, temperature)
     
     print("\n Modelo Carregado com Sucesso!")
     print(f"Época de melhor performance: {epoch_saved + 1}")
@@ -103,8 +136,6 @@ def main():
     
     print("\nBEM-VINDO AO SISTEMA DE DETECÇÃO DE FRAUDE")
     
-    labels_map = {0: "Humano", 1: "IA Pura", 2: "Copy-Typing (Fraude)"}
-
     while True:
         print("-" * 50)
         user_input = input("\nCole um texto para análise (ou digite 'sair' para encerrar):\n> ")
@@ -123,16 +154,40 @@ def main():
         
         confidence = probs[predict_class] * 100
         entropy = get_entropy(probs)
+        veredict = labels_map[predict_class]
 
         print("\nRESULTADO DA ANÁLISE:")
-        print(f"Veredito: {labels_map[predict_class].upper()}")
+        print(f"Veredito: {veredict.upper()}")
         print(f"Certeza : {confidence:.2f}%")
         print(f"Entropia: {entropy:.4f}")
         
-        print("\nProbabilidades Calibradas:")
+        print("\nProbabilidades:")
         print(f"[{probs[0]*100:05.2f}%] Humano")
         print(f"[{probs[1]*100:05.2f}%] IA Pura")
         print(f"[{probs[2]*100:05.2f}%] Copy-Typing")
+
+        print("\nDesconstruindo a decisão com LIME (aguarde alguns segundos)...")
+        try:
+            exp = explainer.explain_instance(
+                user_input, 
+                lime_predict_fn, 
+                labels=[predict_class],
+                num_features=20, 
+                num_samples=5000
+            )
+            
+            now = datetime.now().strftime("%Y%m%d_%H%M%S")
+            file_name = f"lime_{veredict.replace(' ', '_')}_{now}.html"
+            file_path = os.path.join(LIME_OUTPUT_DIR, file_name)
+
+            exp.save_to_file(file_path)
+            print(f"Explicação LIME gerada com sucesso!")
+            
+            file_url = 'file://' + os.path.abspath(file_path)
+            webbrowser.open(file_url)
+            
+        except Exception as e:
+            print(f"Erro ao gerar LIME: {e}")
 
 if __name__ == "__main__":
     main()
